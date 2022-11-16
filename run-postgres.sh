55038baf41687bfda631e149198be98f04261a43
Qobiljon <qobiljon.toshnazarov@gmail.com>
2022-11-16 21:05:47 +0900

    docker compose v2

--- run-postgres.sh
+++ run-postgres.sh
@@ -2,5 +2,5 @@ set -a
 source .env
 set +a
 
-docker-compose -f yamls/postgres.yaml down --rmi all
-docker-compose -f yamls/postgres.yaml up -d
+docker compose -f yamls/postgres.yaml down --rmi all
+docker compose -f yamls/postgres.yaml up -d
--- run-services.sh
+++ run-services.sh
@@ -2,5 +2,5 @@ set -a
 source .env
 set +a
 
-docker-compose -f yamls/services.yaml down --rmi all
-docker-compose -f yamls/services.yaml up -d
+docker compose -f yamls/services.yaml down --rmi all
+docker compose -f yamls/services.yaml up -d
