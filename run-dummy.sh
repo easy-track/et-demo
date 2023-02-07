ec36ac78d9ddf1e3925f056d45141adf05274b64
Qobiljon <qobiljon.toshnazarov@gmail.com>
2023-02-07 11:34:35 +0900

    docker-compose instead of docker compose

--- run-dummy.sh
+++ run-dummy.sh
@@ -2,5 +2,5 @@ set -a
 source .env
 set +a
 
-docker compose -f yamls/dummy.yaml down --rmi all
-docker compose -f yamls/dummy.yaml up -d
+docker-compose -f yamls/dummy.yaml down --rmi all
+docker-compose -f yamls/dummy.yaml up -d
--- stop-all.sh
+++ stop-all.sh
@@ -2,7 +2,7 @@ set -a
 source .env
 set +a
 
-docker compose -f yamls/dashboard.yaml down --rmi all
-docker compose -f yamls/dummy.yaml down --rmi all
-docker compose -f yamls/services.yaml down --rmi all
-docker compose -f yamls/postgres.yaml down --rmi all
+docker-compose -f yamls/dashboard.yaml down --rmi all
+docker-compose -f yamls/dummy.yaml down --rmi all
+docker-compose -f yamls/services.yaml down --rmi all
+docker-compose -f yamls/postgres.yaml down --rmi all
